f9019f734e0488e83fa3b6d95c2b22f30c7ec686
hutor04 <yauhen.khutarniuk@gmail.com>
2021-03-06 20:20:10 +0100

    Added form

--- src/components/learning-resources/TheResources.vue
+++ src/components/learning-resources/TheResources.vue
@@ -10,7 +10,9 @@
                 :mode="addResButtonMode"
         >Add Resources</base-button>
     </base-card>
-    <component :is="selectedTab"></component>
+    <keep-alive>
+        <component :is="selectedTab"></component>
+    </keep-alive>
 </template>
 
 <script>
@@ -43,6 +45,7 @@
     provide() {
       return {
         resources: this.storedResources,
+        addResource: this.addResource,
       };
     },
     computed: {
@@ -56,6 +59,16 @@
     methods: {
       setSelectedTab(tab) {
         this.selectedTab = tab;
+      },
+      addResource(title, description, link) {
+        const newResource = {
+          id: new Date().toISOString(),
+          title: title,
+          description: description,
+          link: link,
+        };
+        this.storedResources.push(newResource);
+        this.selectedTab = 'stored-resources';
       },
     }
   };
